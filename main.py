ef274cbf1e9cd3586222e017719083993f9406b3
Dylan Fisher <dylanfisher@Dylans-MacBook-Air.local>
2024-04-15 19:34:41 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # load .env variables
+
+city = os.getenv('CITY')
+API_key = os.getenv('API_KEY')
+units = 'imperial'
+URL = f'https://api.openweathermap.org/data/2.5/onecall?APPID={API_key}&q={city}&units={units}'
+
+weather_data = requests.get(URL).json()
+
+if __name__ == '__main__':
+    print(weather_data)
