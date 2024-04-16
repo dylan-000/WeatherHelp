f3319c394ab2066a99ccd30aacfe74a9b2128587
dylanfisher <dylanfisher1954@gmail.com>
2024-04-15 22:46:52 -0400

    API is now working and i was able to successfully send a weather forecast to my email.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import certifi
 import requests
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 import schedule
 import smtplib
 import ssl
@@ -12,18 +13,37 @@ load_dotenv()  # load .env variables
 # Weather details
 city = os.getenv('CITY')
 API_key = os.getenv('API_KEY')
-units = 'imperial'
-URL = f'https://api.openweathermap.org/data/2.5/onecall?APPID={API_key}&q={city}&units={units}'
+params = {'q':city, 'appid':API_key, 'units':'imperial'}
+URL = 'http://api.openweathermap.org/data/2.5/weather'
 
-# Store the weather details in a .json file
-weather_data = requests.get(URL).json()
+# access the url
+response = requests.get(URL, params=params)
+
+weather_data = response.json()
+
+#Store data
+min_temp = weather_data['main']['temp_min']
+max_temp = weather_data['main']['temp_max']
+forecast = weather_data['weather'][0]['main']
+sunset = datetime.fromtimestamp(weather_data['sys']['sunset']).time().strftime('%I:%M %p')
+
+#---------------------------------------------------- SMTP CODE
 
 # Email details
 sender_email = os.getenv('FROM')
 receiver_email = os.getenv('TO')
 
 subject = 'Daily weather forecast'
-body = 'Hello'
+
+body = f'''                                                                                              
+Hello,                                                                                                   
+                                                                                                         
+Today the forecast is supposed to be {forecast}, with a minimum temperature of {min_temp}°F              
+and a maximum temperature of {max_temp}°F. The sun will set at {sunset} too.                        
+                                                                                                                                                                                                                  
+Best,                                                                                                    
+Your favorite python-based weatherman                                                                    
+'''
 
 em = EmailMessage()
 em['From'] = sender_email
@@ -38,5 +58,3 @@ context = ssl.create_default_context(cafile=certifi.where())
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(sender_email, os.getenv('PASS'))
     smtp.sendmail(sender_email, receiver_email, em.as_string())
-
-
