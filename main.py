00cd1a7ae6efff0130f97317dd98acde9cf9ad18
dylanfisher <dylanfisher1954@gmail.com>
2024-04-22 09:23:32 -0400

    updated prompt

--- main.py
+++ main.py
@@ -2,43 +2,39 @@ import certifi
 import requests
 from dotenv import load_dotenv
 import os
-from datetime import datetime
 import smtplib
 import ssl
 from email.message import EmailMessage
-import textwrap
 import google.generativeai as genai
-from IPython.display import display
-from IPython.display import Markdown
+
 
 
 load_dotenv()
 
 # Store important data
 google_key = os.getenv('GEMINI_KEY')
-city = os.getenv('CITY')
 weather_key = os.getenv('WEATHER_API_KEY')
+city = os.getenv('CITY')
 params = {'q': city, 'appid': weather_key, 'units': 'imperial'}
 url = 'http://api.openweathermap.org/data/2.5/weather'
 
-# Call API
+# Call weather API and configure Gemini
 weather_data = requests.get(url, params=params).json()
 genai.configure(api_key=google_key)
 model = genai.GenerativeModel('gemini-pro')
 
+# Send text prompt to Gemini
 response = model.generate_content(f"""
 Please generate a message giving a detailed summary of the weather forecast given the following .JSON data:
 {weather_data}
 insert a new line after every sentence.
 """)
-
 message = response.text
-# Email details
+
+# Email formatting
 sender_email = os.getenv('FROM')
 receiver_email = os.getenv('TO')
-
 subject = 'Daily weather forecast'
-
 em = EmailMessage()
 em['From'] = sender_email
 em['To'] = receiver_email
