66fbae2f5f31844234fb9689b91894d3caa3e936
dylanfisher <dylanfisher1954@gmail.com>
2024-04-21 20:19:24 -0400

    Implimented functionality with google Gemini

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
 import certifi
 import requests
-import schedule
 from dotenv import load_dotenv
 import os
 from datetime import datetime
-import time
-from schedule import every, repeat
 import smtplib
 import ssl
 from email.message import EmailMessage
-
 import textwrap
-
 import google.generativeai as genai
-
 from IPython.display import display
 from IPython.display import Markdown
 
 
-load_dotenv()  # load .env variables
+load_dotenv()
 
 # Store important data
 google_key = os.getenv('GEMINI_KEY')
 city = os.getenv('CITY')
-weather_key = os.getenv('API_KEY')
+weather_key = os.getenv('WEATHER_API_KEY')
 params = {'q': city, 'appid': weather_key, 'units': 'imperial'}
 url = 'http://api.openweathermap.org/data/2.5/weather'
 
-# Access the url and turn response into .json
+# Call API
 weather_data = requests.get(url, params=params).json()
+genai.configure(api_key=google_key)
+model = genai.GenerativeModel('gemini-pro')
 
-# Store data
-min_temp = weather_data['main']['temp_min']
-max_temp = weather_data['main']['temp_max']
-forecast = weather_data['weather'][0]['main']
-sunset = datetime.fromtimestamp(weather_data['sys']['sunset']).time().strftime('%I:%M %p')
+response = model.generate_content(f"""
+Please generate a message giving a detailed summary of the weather forecast given the following .JSON data:
+{weather_data}
+""")
+
+message = response.text
 
 # Email details
 sender_email = os.getenv('FROM')
@@ -42,21 +39,11 @@ receiver_email = os.getenv('TO')
 
 subject = 'Daily weather forecast'
 
-body = f'''                                                                                              
-Hello,                                                                                                   
-                                                                                                         
-Today the forecast is supposed to be {forecast}, with a minimum temperature of {min_temp}°F              
-and a maximum temperature of {max_temp}°F.  The sun will set at {sunset} too.                        
-                                                                                                                                                                                                                  
-Best,                                                                                                    
-Your favorite python-based weatherman                                                                    
-'''
-
 em = EmailMessage()
 em['From'] = sender_email
 em['To'] = receiver_email
 em['Subject'] = subject
-em.set_content(body)
+em.set_content(message)
 
 # Add SSL
 context = ssl.create_default_context(cafile=certifi.where())
